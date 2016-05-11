8cc905acf72898a2eefd2103e3e9550fc13e70d4
Keisha Heeralal-Crooks <keishaheeralal@gmail.com>
2016-05-11 14:28:25 -0400

    Add modal dialog

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-function tabNavigation(e){
+function tabNavigation(e) {
 
   /*get element that's currently active and remove*/
   var active = document.getElementsByClassName("active");
@@ -8,7 +8,6 @@ function tabNavigation(e){
   e.target.parentNode.classList.add("active");
 
   /*get 'visible' content and hide*/
-  //debugger
   var visible = document.getElementsByClassName("visible");
   var element = visible[0];
   element.classList.remove("visible");
@@ -23,9 +22,46 @@ function tabNavigation(e){
   clickedContent.classList.add("visible");
 }
 
+function modalDialog(e) {
 
-var el = document.getElementsByClassName("tab-menu");
-var i;
-for (i = 0; i < el.length; i++) {
-    el[i].addEventListener("click", tabNavigation, false);
+  /*get content of menu item clicked and change in HTML*/
+  var clicked = document.getElementById("clicked");
+  clicked.innerHTML = "'" + e.target.text + "'";
+
+  /*set dialog to display*/
+  var dialog = document.getElementsByClassName("close");
+  dialog[0].classList.remove("close");
+
+  /*add screen behind dialog*/
+  var dialogScreen = document.getElementById("modalBackground");
+  dialogScreen.classList.add("modal-background");
 }
+
+function closeDialog(e) {
+
+  /*hide dialog*/
+  var dialog = document.getElementsByClassName("modal-dialog");
+  dialog[0].classList.add("close");
+
+  /*hide screen*/
+  var dialogScreen = document.getElementById("modalBackground");
+  dialogScreen.classList.remove("modal-background");
+}
+
+/*add click event listeners for tab navigation*/
+var elTab = document.getElementsByClassName("tab-menu");
+var i;
+for (i = 0; i < elTab.length; i++) {
+    elTab[i].addEventListener("click", tabNavigation, false);
+}
+
+/*add click event listeners for side menu*/
+var elSide = document.getElementsByClassName("side-menu");
+var j;
+for (j = 0; j < elSide.length; j++) {
+    elSide[j].addEventListener("click", modalDialog, false);
+}
+
+/*add click event listener for X on dialog box*/
+var elA = document.getElementById("close");
+elA.addEventListener("click", closeDialog, false);
